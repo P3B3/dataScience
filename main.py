ef14c960ad02046ec17e405d49740792dabe8180
George <georgii.konyaev@tutanota.com>
2018-04-06 18:17:42 +0300

    type fix

--- main.py
+++ main.py
@@ -8,10 +8,10 @@ low_memory = False
 
 numericFile = pd.read_csv(surveyNumeric)
 valueFile = pd.read_csv(surveyValues)
-respondentsRF = valueFile['CountryNumeric2'].value_counts().ix['Russian Federation']
+respondentsRF = valueFile['CountryNumeric2'].value_counts().ix['Russian Federation'].astype(int)
 print('Всего из РФ: ', respondentsRF)
-studentsFromRF = numericFile.loc[(numericFile['CountryNumeric2'] == 138) & (numericFile['q8Student'] == 1)]
-studentsFromRF = studentsFromRF['q8Student'].value_counts().ix[1]
+studentsFromRF = (numericFile.loc[(numericFile['CountryNumeric2'] == 138) &
+                                  (numericFile['q8Student'] == 1)])['q8Student'].value_counts().ix[1].astype(int)
 print('Процент студентов из РФ: ', float('{:.2f}'.format((studentsFromRF / respondentsRF) * 100)))
 
 
