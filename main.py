a8b867a1af374a17a529d0cfc9d2675db1620f15
George <georgii.konyaev@tutanota.com>
2018-04-06 18:32:37 +0300

    add percent function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import csv
+from percent import percentage_of
 from constValues import countryCode, surveyNumeric, \
     surveyCodebook, surveyNumericMapping, surveyValues
 
@@ -12,7 +13,7 @@ respondentsRF = valueFile['CountryNumeric2'].value_counts().ix['Russian Federati
 print('Всего из РФ: ', respondentsRF)
 studentsFromRF = (numericFile.loc[(numericFile['CountryNumeric2'] == 138) &
                                   (numericFile['q8Student'] == 1)])['q8Student'].value_counts().ix[1].astype(int)
-print('Процент студентов из РФ: ', float('{:.2f}'.format((studentsFromRF / respondentsRF) * 100)))
+print('Процент студентов из РФ: ', float('{:.2f}'.format(percentage_of(studentsFromRF, respondentsRF))))
 
 
 # 138 код РФ
---
+++ percent.py
@@ -0,0 +1,3 @@
+def percentage_of(part, total):
+    percent = (part / total) * 100
+    return percent
