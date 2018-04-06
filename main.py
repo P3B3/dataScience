16724a20d3d704e8458fb2f2d6a580a653d8ee24
George <georgii.konyaev@tutanota.com>
2018-04-06 14:05:39 +0300

    add count+data

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
 import pandas as pd
 import csv
+from constValues import countryCode, surveyNumeric, \
+    surveyCodebook, surveyNumericMapping, surveyValues
 
+low_memory = False
 
-countryCode = 'Country-Code-Mapping.csv'
+ValueFile = pd.read_csv(surveyValues)
+newValueFile = ValueFile['CountryNumeric2'].value_counts()
+print('Количество опрошенных из РФ:', newValueFile.ix['Russian Federation'])
 
-printFile = pd.read_csv(countryCode)
-
-print(printFile)
