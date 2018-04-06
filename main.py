9c6a7a0bc2db63343a58499cda16ec17ccabc8f8
P3B3 <peanut_butter@tutanota.com>
2018-04-07 01:43:15 +0300

    add format func

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import csv
 from percent import percentage_of
+from formatNum import format_float_num
 from constValues import countryCode, surveyNumeric, \
     surveyCodebook, surveyNumericMapping, surveyValues
 
@@ -15,15 +16,15 @@ print('Всего из РФ: ', respondentsRF)
 
 studentsFromRF = (numericFile.loc[(numericFile['CountryNumeric2'] == 138) &
                                   (numericFile['q8Student'] == 1)])['q8Student'].value_counts().ix[1].astype(int)
-print('Процент студентов из РФ: ', float('{:.2f}'.format(percentage_of(studentsFromRF, respondentsRF))))
+print('Процент студентов из РФ: ', format_float_num(percentage_of(studentsFromRF, respondentsRF)))
 
 respondentsRoles = numericFile['q9CurrentRole'].value_counts().sum()
 webDeveloperRoles = numericFile['q9CurrentRole'].value_counts().ix['1'].astype(int)
 dataScientistRoles = numericFile['q9CurrentRole'].value_counts().ix['6'].astype(int)
 mobileDeveloperRoles = numericFile['q9CurrentRole'].value_counts().ix['7'].astype(int)
-print('Процент веб разработчиков: ', float('{:.2f}'.format(percentage_of(webDeveloperRoles, respondentsRoles))))
-print('Процент дата разработчиков: ', float('{:.2f}'.format(percentage_of(dataScientistRoles, respondentsRoles))))
-print('Процент моб разработчиков: ', float('{:.2f}'.format(percentage_of(mobileDeveloperRoles, respondentsRoles))))
+print('Процент веб разработчиков: ', format_float_num(percentage_of(webDeveloperRoles, respondentsRoles)))
+print('Процент дата разработчиков: ', format_float_num(percentage_of(dataScientistRoles, respondentsRoles)))
+print('Процент моб разработчиков: ', format_float_num(percentage_of(mobileDeveloperRoles, respondentsRoles)))
 
-trueAnswersRF = numericFile.loc[(numericFile['CountryNumeric2'] == 138)].count(axis=1).min().astype(int)
-print('Процент правильно ответивших из РФ: ', float('{:.2f}'.format(percentage_of(trueAnswersRF, respondentsRF))))
+trueAnswersRF = numericFile.loc[numericFile['CountryNumeric2'] == 138].count(axis=1).min().astype(int)
+print('Процент правильно ответивших из РФ: ', format_float_num(percentage_of(trueAnswersRF, respondentsRF)))
