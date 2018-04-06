41afcbfa200abfae130645002c8ddc8f6775d1f4
Peanut <20405541+P3B3@users.noreply.github.com>
2018-04-06 14:13:27 +0300

    add count+data (#2)

---
+++ constValues.py
@@ -0,0 +1,14 @@
+# соответствие стран и их кода
+countryCode = 'data/Country-Code-Mapping.csv'
+
+# схема опроса
+surveyCodebook = 'data/HackerRank-Developer-Survey-2018-Codebook.csv'
+
+#
+surveyNumeric = 'data/HackerRank-Developer-Survey-2018-Numeric.csv'
+
+#
+surveyNumericMapping = 'data/HackerRank-Developer-Survey-2018-Numeric-Mapping.csv'
+
+#
+surveyValues = 'data/HackerRank-Developer-Survey-2018-Values.csv'
